5acd2654b4403c7106f70aaa52a745961e2e07bd
Artem <a.khimin@yandex.ru>
2021-08-19 02:08:13 +0300

    делегат ParameterizedThreadStart

--- Multithreading/Program.cs
+++ Multithreading/Program.cs
@@ -36,6 +36,38 @@ namespace Multithreading
             }
 
             Console.ReadLine();
+            //*************************************
+
+            int number = 4;
+            // создаем новый поток
+            Thread myThread2 = new Thread(new ParameterizedThreadStart(Count2));
+            myThread2.Start(number);
+
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("Главный поток:");
+                Console.WriteLine(i * i);
+                Thread.Sleep(300);
+            }
+
+            Console.ReadLine();
+
+            //*******************************
+
+            Counter counter = new Counter();
+            counter.x = 4;
+            counter.y = 5;
+
+            Thread myThread3 = new Thread(new ParameterizedThreadStart(Count3));
+            myThread3.Start(counter);
+
+            //*********************
+            Console.ReadKey();
+            Counter4 counter4 = new Counter4(5, 4);
+
+            Thread myThread4 = new Thread(new ThreadStart(counter4.Count4));
+            myThread4.Start();
+
         }
 
         public static void Count()
@@ -44,6 +76,57 @@ namespace Multithreading
             {
                 Console.WriteLine("Второй поток:");
                 Console.WriteLine(i * i);
+                Thread.Sleep(400);
+            }
+        }
+
+        public static void Count2(object x)
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                int n = (int)x;
+
+                Console.WriteLine("Второй поток 2:");
+                Console.WriteLine(i * n);
+                Thread.Sleep(400);
+            }
+        }
+
+        public static void Count3(object obj)
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                Counter c = (Counter)obj;
+
+                Console.WriteLine("Второй поток 3:");
+                Console.WriteLine(i * c.x * c.y);
+            }
+        }
+    }
+
+    public class Counter
+    {
+        public int x;
+        public int y;
+    }
+
+    public class Counter4
+    {
+        private int x;
+        private int y;
+
+        public Counter4(int _x, int _y)
+        {
+            this.x = _x;
+            this.y = _y;
+        }
+
+        public void Count4()
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("Второй поток 4:");
+                Console.WriteLine(i * x * y);
                 Thread.Sleep(400);
             }
         }
