3bab38220e89ad92ab83dac1be150581db65c071
Artem <a.khimin@yandex.ru>
2021-08-18 23:44:15 +0300

    тетс потоков

--- Multithreading/Program.cs
+++ Multithreading/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Multithreading
 {
@@ -6,7 +7,45 @@ namespace Multithreading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // получаем текущий поток
+            Thread t = Thread.CurrentThread;
+
+            //получаем имя потока
+            Console.WriteLine($"Имя потока: {t.Name}");
+            t.Name = "Метод Main";
+            Console.WriteLine($"Имя потока: {t.Name}");
+
+            Console.WriteLine($"Запущен ли поток: {t.IsAlive}");
+            Console.WriteLine($"Приоритет потока: {t.Priority}");
+            Console.WriteLine($"Статус потока: {t.ThreadState}");
+
+            // получаем домен приложения
+            Console.WriteLine($"Домен приложения: {Thread.GetDomain().FriendlyName}");
+
+            Console.ReadLine();
+
+            // создаем новый поток
+            Thread myThread = new Thread(new ThreadStart(Count));
+            myThread.Start(); // запускаем поток
+
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("Главный поток:");
+                Console.WriteLine(i * i);
+                Thread.Sleep(300);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void Count()
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("Второй поток:");
+                Console.WriteLine(i * i);
+                Thread.Sleep(400);
+            }
         }
     }
 }
