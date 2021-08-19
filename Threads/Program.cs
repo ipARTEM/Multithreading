1e870f5c5fada4fc44d8a38de5509db070aa25be
Artem <a.khimin@yandex.ru>
2021-08-19 22:30:50 +0300

    Threads

---
+++ Threads/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class Program
+    {
+
+        static void Secondary()
+        {
+            while (true)
+            {
+                Console.WriteLine(new string(' ', 20) + "Вторичный поток");
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            ThreadStart threadTwo = new ThreadStart(Secondary);
+
+            Thread thread = new Thread(threadTwo);
+            thread.Start();
+
+
+
+            while (true)
+            {
+                Console.WriteLine("Первичный поток");
+            }
+        }
+    }
+}
