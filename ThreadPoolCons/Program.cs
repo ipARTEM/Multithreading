3281f77881b33efe10563ea5bf1db5ae36757e76
Artem <a.khimin@yandex.ru>
2021-09-21 12:01:14 +0300

    Тест ThreadPool

---
+++ ThreadPoolCons/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPoolCons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread thread = new Thread(Work);
+                thread.Start(i);
+
+                Thread.Sleep(200);
+
+            }
+            Console.WriteLine("*******************");
+            Console.ReadLine();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(Work, i);
+
+                Thread.Sleep(200);
+
+            }
+            Console.WriteLine("*******************");
+            Console.ReadLine();
+        }
+
+        private static void Work(object i)
+        {
+            Console.WriteLine($" Идентификатор потока: {Thread.CurrentThread.ManagedThreadId}, параметр: {i}");
+        }
+    }
+}
